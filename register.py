f0336a3ad16cf8dd2d33f55d5b01afbfc808dfd4
DmitriyShum <dmitriyshumkin@me.com>
2018-10-22 00:31:26 -0400

    registeraccount.py
    updated this script for people to make an account

--- register.py
+++ register.py
@@ -5,3 +5,5 @@ usrname = input("Please enter a username:")
 print(usrname, " Registered.")
 pssword = input("Please make a password:")
 print("Password registered.")
+print("Thank you for making an account with us, ", usrname, "!")
+#End of script
